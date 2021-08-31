98dcaf3e7622d50a86e5a7a5bf95f964d925b402
luciana-pereira <luci.lv14@gmail.com>
2021-08-30 22:35:06 -0300

    vtex api.

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,10 +1,10 @@
-import { IOClients } from '@vtex/api'
+import { IOClients } from '@vtex/api';
 
-import Status from './status'
+import Leads from './leads';
 
-// Extend the default IOClients implementation with our own custom clients.
-export class Clients extends IOClients {
-  public get status() {
-    return this.getOrSet('status', Status)
+export class  Clients extends IOClients {
+  public get listLeads() {
+    return this.getOrSet('listLeads', Leads)
   }
 }
+
